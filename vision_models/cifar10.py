6b5a22047e400d56b8f69e6b31ccff0d16cc141c
Varun Suresh <fab.varun@gmail.com>
2025-01-09 22:28:36 -0800

    Small cleanup

--- vision_models/cifar10.py
+++ vision_models/cifar10.py
@@ -11,15 +11,14 @@ class cifar10(Dataset):
         self.data = load_dataset("uoft-cs/cifar10")[split]
         self.mean = torch.tensor([0.4914, 0.4822, 0.4465])
         self.std = torch.tensor([0.2023, 0.1994, 0.2010])
-        self.train_transforms = v2.Compose([#v2.ToDtype(torch.float32,scale=True),
+        self.train_transforms = v2.Compose([
                                             v2.RandomCrop(size=(32,32),padding=4),
                                             v2.RandomHorizontalFlip(0.5),
                                             v2.Normalize(mean=self.mean,std=self.std),
                                             ])
 
-        self.test_transforms = v2.Compose([#v2.ToDtype(torch.float32, scale=True),
-                                           v2.Normalize(mean=self.mean,std=self.std),
-                                           ])
+        self.test_transforms = v2.Compose([v2.Normalize(mean=self.mean,std=self.std)])
+
     def __len__(self):
         return len(self.data)
 
