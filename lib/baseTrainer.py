17e07d657bffb47b4aea021f0cc801cf95118240
Varun Suresh <fab.varun@gmail.com>
2025-01-09 22:28:12 -0800

    Works except the test set error

--- lib/baseTrainer.py
+++ lib/baseTrainer.py
@@ -7,11 +7,11 @@ Base Trainer Class with default methods to
 """
 from abc import ABC,abstractmethod
 from dataclasses import asdict
-from torch.utils.data.dataloader import DataLoader
+from typing import Dict
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
-from resnet_config import ResNetCIFAR10Config
-from resnet import ResNetCifar
+from vision_models.resnet_config import ResNetCIFAR10Config
+from vision_models.resnet import ResNetCifar
 MODELS = {"resnet-cifar": ResNetCifar}
 MODEL_CONFIGS = {"resnet-cifar": ResNetCIFAR10Config}
 import os
@@ -30,10 +30,10 @@ class BaseTrainer(ABC):
         """
         Loads the model based on the config
         """
-        if self.train_config.init_from == "resume":
-            ckpt_path = os.path.join(self.train_config.out_dir,self.train_config.checkpoint_name)
+        if self.config.init_from == "resume":
+            ckpt_path = os.path.join(self.config.out_dir,self.config.checkpoint_name)
             print(f"Resuming training from {ckpt_path}")
-            self.ckpt = torch.load(ckpt_path, map_location=self.train_config.device)
+            self.ckpt = torch.load(ckpt_path, map_location=self.config.device)
             model_config = MODEL_CONFIGS.get(self.config.model_type)(**self.ckpt["model_config"])
             #Update some params
             model_config.load_from_checkpoint = self.model_config.load_from_checkpoint
@@ -42,18 +42,23 @@ class BaseTrainer(ABC):
             self.model = MODELS.get(self.config.model_type)(self.model_config.n)
             self.model.load_state_dict(self.ckpt["model"])
         else:
+            self.model_config = MODEL_CONFIGS.get(self.config.model_type)()
             self.model = MODELS.get(self.config.model_type)(self.model_config.n)
-        self.model.to(self.train_config.device)
+        self.model.to(self.config.device)
 
-    def load_optimizer(self):
+    def load_optimizer_scheduler(self):
         """
         By default, uses a AdamW optimizer
         """
         self.optimizer = torch.optim.AdamW(self.model.parameters(),
-            lr=self.train_config.learning_rate,
-            betas=(self.train_config.beta1,self.train_config.beta2))
-        if self.train_config.init_from == "resume":
+            lr=self.config.learning_rate,
+            betas=(self.config.beta1,self.config.beta2))
+        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer)
+        if self.config.init_from == "resume":
             self.optimizer.load_state_dict(self.ckpt["optimizer"])
+            self.scheduler.load_state_dict(self.ckpt["scheduler"])
+        self.optimizer.zero_grad()
+    
 
     @abstractmethod 
     def freeze_layers(self):
@@ -78,12 +83,12 @@ class BaseTrainer(ABC):
 
     def train(self):
         self.load_model()
-        self.load_optimizer()
+        self.load_optimizer_scheduler()
         self.model.train()
-        if self.train_config.freeze_layers > 0:
-            self.freeze_layers(self.train_config.freeze_layers)
+        if self.config.freeze_layers > 0:
+            self.freeze_layers(self.config.freeze_layers)
 
-        if self.train_config.init_from == "resume":
+        if self.config.init_from == "resume":
             start_iter = self.ckpt["iter_num"]
             best_val_loss = self.ckpt["best_val_loss"]
         else:
@@ -91,78 +96,100 @@ class BaseTrainer(ABC):
             best_val_loss = 1e9
         
         dl = self.create_dataloader(self.train_set) 
-        accumulation_steps = self.train_config.batch_size // self.train_config.micro_batch_size
+        accumulation_steps = self.config.batch_size // self.config.micro_batch_size
 
-        for self.iter_num in tqdm(range(start_iter, self.train_config.max_iters)):
-            if self.train_config.grad_clip != 0.0:
-                torch.nn.utils.clip_grad_norm_(self.model.parameters(),self.train_config.grad_clip)
+        for self.iter_num in tqdm(range(start_iter, self.config.max_iters)):
+            if self.config.grad_clip != 0.0:
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(),self.config.grad_clip)
             
             if self.iter_num % accumulation_steps == 0:
                 self.optimizer.step()
                 self.optimizer.zero_grad()
             
-            if self.iter_num % self.train_config.eval_interval == 0:
-                losses = self.estimate_losses()
-                test_error = self.calculate_test_error()
+            if self.iter_num % self.config.eval_interval == 0:
+                # losses = self.estimate_losses()
+                losses = {"train":0,"val":0}
+                errors = self.calculate_error()
                 print(f"Step: {self.iter_num}\nTrain Loss: {losses['train']}\nValidation Loss:{losses['val']}")
-                self.writer.add_scalar("Loss/train",losses["train"], self.iter_num)
-                self.writer.add_scalar("Loss/val", losses["val"],self.iter_num)
-                self.writer.add_scalar("Test Error",test_error,self.iter_num)
+                print(f"Train Error: {errors['train']}\nValidation Error: {errors['val']}\nTest Error: {errors['test']}")
+                [self.writer.add_scalar(f"Loss/{split}", loss,self.iter_num) for split,loss in losses.items()]
+                [self.writer.add_scalar(f"{split} error",error,self.iter_num) for split,error in errors.items()]
                  
-                if losses["val"] < best_val_loss or self.train_config.always_save_checkpoint:
+                if losses["val"] < best_val_loss or self.config.always_save_checkpoint:
                     best_val_loss = losses["val"]
                     if self.iter_num > start_iter:
                         ckpt = {"model": self.model.state_dict(),
-                        "train_config": asdict(self.train_config),
+                        "config": asdict(self.config),
                         "model_config": asdict(self.model_config),
                         "optimizer": self.optimizer.state_dict(),
+                        "scheduler": self.scheduler.state_dict(),
                         "iter_num": self.iter_num,
                         "best_val_loss": best_val_loss}
-                        output_path = os.path.join(self.train_config.out_dir,self.train_config.checkpoint_name)
+                        output_path = os.path.join(self.config.out_dir,self.config.checkpoint_name)
                         print(f"Saving checkpoint to {output_path}")
-                        if not os.path.exists(self.train_config.out_dir):
-                            os.makedirs(self.train_config.out_dir)
+                        if not os.path.exists(self.config.out_dir):
+                            os.makedirs(self.config.out_dir)
                         torch.save(ckpt, output_path)
             
             batch = next(iter(dl))
             model_output = self.run_inference(batch)
-            loss = self.criterion(model_output, batch["label"].to(self.train_config.device)) / (self.train_config.micro_batch_size * accumulation_steps)
+            loss = self.criterion(model_output, batch["label"].to(self.config.device)) / (self.config.micro_batch_size * accumulation_steps)
             loss.backward()
  
-    def estimate_losses(self):
+    def estimate_losses(self)->Dict[str,float]:
         self.model.eval()
         train_dl = self.create_dataloader(self.train_set)
-        val_dl = self.create_dataloader(self.val_dl)
+        val_dl = self.create_dataloader(self.val_set)
         train_loss = 0
         val_loss = 0
         with torch.no_grad():
-            for i in range(self.train_config.eval_iters):
+            for i in range(self.config.eval_iters):
                 train_batch = next(iter(train_dl))
                 train_output = self.run_inference(train_batch) 
-                train_loss += self.criterion(train_output,torch.tensor(train_batch["label"]).to(self.train_config.device))
+                train_loss += self.criterion(train_output,torch.tensor(train_batch["label"]).to(self.config.device))
                 val_batch = next(iter(val_dl))
                 val_output = self.run_inference(val_batch) 
-                val_loss += self.criterion(val_output,val_batch["label"].to(self.train_config.device))
+                val_loss += self.criterion(val_output,val_batch["label"].to(self.config.device))
         
         losses = {}
-        losses["train"] = train_loss/(self.train_config.eval_iters * self.train_config.micro_batch_size)
-        losses["val"] = val_loss/(self.train_config.eval_iters * self.train_config.micro_batch_size)
+        losses["train"] = train_loss/(self.config.eval_iters * self.config.micro_batch_size)
+        losses["val"] = val_loss/(self.config.eval_iters * self.config.micro_batch_size)
         self.model.train()
         return losses
 
-    def calculate_test_error(self):
+    def calculate_error(self)->Dict[str,float]:
         """
         The default method assumes it is a multi-class classification problem
         """
         self.model.eval()
-        test_dl = self.create_dataloader(self.test_set) 
-        correct = 0
-        for batch in test_dl:
-            with torch.no_grad():
-                model_output = self.run_inference(batch) 
-                predictions = torch.argmax(model_output,dim=1)
-                labels = torch.tensor(batch["label"]).to(self.train_config.device)
-                correct += torch.eq(predictions,labels).sum()
-        test_error = 1-correct/len(self.test_set)
+
+        def create_subset(dataset):
+            subset = torch.utils.data.Subset(dataset,indices=torch.randint(high=len(dataset)-1,size=(self.config.eval_size,1)))
+            return subset
+
+        train_subset = create_subset(self.train_set)
+        test_subset = create_subset(self.test_set)
+        val_subset = create_subset(self.val_set)
+
+        # pairs = [("train",train_subset),("val",val_subset),("test",test_subset)]
+        pairs = [("test",test_subset)] 
+        def calculate_subset_error(dataset):
+            correct = 0
+            dl = self.create_dataloader(dataset)
+            print(dataset[0])
+            for batch in dl:
+                print(batch.keys())
+                with torch.no_grad():
+                    model_output = self.run_inference(batch) 
+                    predictions = torch.argmax(model_output,dim=1)
+                    labels = torch.tensor(batch["label"]).to(self.config.device)
+                    correct += torch.eq(predictions,labels).sum()
+            error = 1-correct/len(dataset)
+            return error
+
+        errors = {}
+        for split,subset in pairs:
+            print(split)
+            errors[split] = calculate_subset_error(subset) 
         self.model.train()
-        return test_error  
+        return errors 
