61c02115ed66bd3fabf31db836cce9ae77046247
Varun Suresh <fab.varun@gmail.com>
2025-01-12 17:00:58 -0800

    Move out the config for sentence bert into a separate file

---
+++ language_models/sentenceBERT_config.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from language_models.bert_config import BERTConfig, BERTTrainConfig
+
+@dataclass
+class sentenceBERTConfig(BERTConfig):
+    model_type:str = "sentence-bert"
+    n_classes:int = 3
+
+@dataclass
+class sentenceBERTTrainConfig(BERTTrainConfig):
+    model_type:str = "sentence-bert"
+    checkpoint_name: str = "sbert_ckpt.pt"
+    warmup_iters:int = 12000
+    step_size:int = 120000
