7d18c6b7e22798c687024d10aa7dc2aaf9bab5c8
Varun Suresh <fab.varun@gmail.com>
2025-01-26 21:51:32 -0800

    Crop length of tokenized input

--- language_models/utils/bert_utils.py
+++ language_models/utils/bert_utils.py
@@ -12,10 +12,11 @@ class sentence:
     def __post_init__(self):
         self.attention_mask = self.attention_mask.bool()
 
-def pad_sentences(s):
-    input_ids = [x.input_ids[0] for x in s]
-    token_type_ids = [x.token_type_ids[0] for x in s]
-    attention_mask = [x.attention_mask[0] for x in s]
+def pad_sentences(s,max_length):
+    length = min(max_length, max([len(x.input_ids[0]) for x in s]))
+    input_ids = [x.input_ids[0][0:length] for x in s]
+    token_type_ids = [x.token_type_ids[0][0:length] for x in s]
+    attention_mask = [x.attention_mask[0][0:length] for x in s]
     # print(input_ids)
     input_ids_padded = pad_sequence(input_ids, batch_first=True,padding_value=0)
     token_type_ids_padded = pad_sequence(token_type_ids, batch_first=True, padding_value=0)
@@ -35,8 +36,8 @@ def dynamic_padding(data):
 
     return {"sentence1": s1_padded, "sentence2": s2_padded,"label": label}
 
-def dynamic_padding_scbert(data,max_length=256):
+def dynamic_padding_scbert(data,max_length=512):
     s1 = [tokenizer(item["review"],return_tensors="pt") for item in data]
-    s1_padded = sentence(**pad_sentences(s1))
+    s1_padded = sentence(**pad_sentences(s1,max_length))
     label = torch.tensor([item["label"] for item in data])
     return {"review":s1_padded,"label":label}
