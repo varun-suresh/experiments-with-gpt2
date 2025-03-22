86f3146b3e67b92d7423f9c1009c2c947b5649b4
Varun Suresh <fab.varun@gmail.com>
2025-03-21 17:59:53 -0700

    refactor

--- vision_models/cifar10.py
+++ vision_models/cifar10.py
@@ -12,7 +12,7 @@ class cifar10(Dataset):
         self.mean = torch.tensor([0.4914, 0.4822, 0.4465])
         self.std = torch.tensor([0.2470, 0.2435, 0.2615])
         self.train_transforms = v2.Compose([
-                                            v2.RandomCrop(size=(32,32),padding=4),
+                                            v2.RandomCrop(size=32,padding=4),
                                             v2.RandomHorizontalFlip(0.5),
                                             v2.Normalize(mean=self.mean,std=self.std),
                                             ])
