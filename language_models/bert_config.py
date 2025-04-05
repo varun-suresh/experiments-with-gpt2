09ab520f10af24701741c7b2de39a06571576957
Varun Suresh <fab.varun@gmail.com>
2025-04-04 22:16:00 -0700

    Lint

--- language_models/bert_config.py
+++ language_models/bert_config.py
@@ -1,26 +1,26 @@
 from dataclasses import dataclass
 from typing import Tuple
 
+
 @dataclass
 class BERTConfig:
-
     vocab_size: int = 30522
     embedding_size: int = 768
-    n_heads:int = 12
-    n_layers:int = 12
+    n_heads: int = 12
+    n_layers: int = 12
     pretrained_block_size: int = 512
-    block_size:int = 256
+    block_size: int = 256
     # Load from a checkpoint
-    load_from_checkpoint : bool = False
-    checkpoint_path : str = ""
-    model_type:str = "bert"
-    is_causal:bool = False
+    load_from_checkpoint: bool = False
+    checkpoint_path: str = ""
+    model_type: str = "bert"
+    is_causal: bool = False
     layer_norm_eps: float = 1e-12
     # Training specific params:
     # LoRA params
-    use_lora:bool = True
-    r:int = 8
-    lora_layers: Tuple = (10,11)
+    use_lora: bool = True
+    r: int = 8
+    lora_layers: Tuple = (10, 11)
 
     # Regularizaztion
     dropout: float = 0.1
@@ -32,38 +32,40 @@ class BERTConfig:
 @dataclass
 class BERTTrainConfig:
     # I/O
-    out_dir:str = "out"
+    out_dir: str = "out"
     model_type: str = "sc-bert"
     checkpoint_name: str = "bert_ckpt_train_warmup.pt"
-    eval_interval:int = 2000
-    eval_size:int = 2000
-    eval_iters:int = 100
-    eval_only:bool = False
-    
-    init_from:str = "bert" # 'bert' or 'resume' - it will resume from the latest checkpoint
-    always_save_checkpoint:bool = True
+    eval_interval: int = 2000
+    eval_size: int = 2000
+    eval_iters: int = 100
+    eval_only: bool = False
+
+    init_from: str = (
+        "bert"  # 'bert' or 'resume' - it will resume from the latest checkpoint
+    )
+    always_save_checkpoint: bool = True
 
     # data
-    batch_size:int = 16
+    batch_size: int = 16
 
     # AdamW optimizer
-    learning_rate:float = 2e-5
-    max_iters:int = 5000
-    beta1:float = 0.9
-    beta2:float = 0.999
-    grad_clip:float = 1.0
+    learning_rate: float = 2e-5
+    max_iters: int = 5000
+    beta1: float = 0.9
+    beta2: float = 0.999
+    grad_clip: float = 1.0
 
     # # Learning Rate scheduler : StepLR
-    warmup_iters:int = 12000
-    step_size:int = 120000
+    warmup_iters: int = 12000
+    step_size: int = 120000
     # lr_decay_iters:int = 40000
     # min_lr: float = 6e-5
 
-    #device
-    device:str = "cuda"
+    # device
+    device: str = "cuda"
 
     # Gradient Accumulation
-    micro_batch_size:int = 8 
+    micro_batch_size: int = 8
 
     # Freeze layers when fine-tuning
-    freeze_layers:int = 0
+    freeze_layers: int = 0
--- language_models/utils/bert_utils.py
+++ language_models/utils/bert_utils.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from torch.nn.utils.rnn import pad_sequence
 from transformers import BertTokenizer
 
+
 @dataclass
 class sentence:
     input_ids: torch.Tensor
@@ -12,32 +13,44 @@ class sentence:
     def __post_init__(self):
         self.attention_mask = self.attention_mask.bool()
 
-def pad_sentences(s,max_length):
+
+def pad_sentences(s, max_length):
     length = min(max_length, max([len(x.input_ids[0]) for x in s]))
     input_ids = [x.input_ids[0][0:length] for x in s]
     token_type_ids = [x.token_type_ids[0][0:length] for x in s]
     attention_mask = [x.attention_mask[0][0:length] for x in s]
     # print(input_ids)
-    input_ids_padded = pad_sequence(input_ids, batch_first=True,padding_value=0)
-    token_type_ids_padded = pad_sequence(token_type_ids, batch_first=True, padding_value=0)
-    attention_mask_padded = pad_sequence(attention_mask,batch_first=True,padding_value=0)
+    input_ids_padded = pad_sequence(input_ids, batch_first=True, padding_value=0)
+    token_type_ids_padded = pad_sequence(
+        token_type_ids, batch_first=True, padding_value=0
+    )
+    attention_mask_padded = pad_sequence(
+        attention_mask, batch_first=True, padding_value=0
+    )
 
-    return {"input_ids": input_ids_padded, "token_type_ids":token_type_ids_padded, "attention_mask": attention_mask_padded}
+    return {
+        "input_ids": input_ids_padded,
+        "token_type_ids": token_type_ids_padded,
+        "attention_mask": attention_mask_padded,
+    }
+
 
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
+
 def dynamic_padding(data):
-    s1 = [tokenizer(item["sentence1"],return_tensors="pt") for item in data]
-    s2 = [tokenizer(item["sentence2"],return_tensors="pt") for item in data]
+    s1 = [tokenizer(item["sentence1"], return_tensors="pt") for item in data]
+    s2 = [tokenizer(item["sentence2"], return_tensors="pt") for item in data]
     s1_padded = sentence(**pad_sentences(s1))
     s2_padded = sentence(**pad_sentences(s2))
     label = torch.tensor([item["score"] for item in data])
     # label = torch.tensor([item["label"] for item in data])
 
-    return {"sentence1": s1_padded, "sentence2": s2_padded,"label": label}
+    return {"sentence1": s1_padded, "sentence2": s2_padded, "label": label}
 
-def dynamic_padding_scbert(data,max_length=256):
-    s1 = [tokenizer(item["review"],return_tensors="pt") for item in data]
-    s1_padded = sentence(**pad_sentences(s1,max_length))
+
+def dynamic_padding_scbert(data, max_length=256):
+    s1 = [tokenizer(item["review"], return_tensors="pt") for item in data]
+    s1_padded = sentence(**pad_sentences(s1, max_length))
     label = torch.tensor([item["label"] for item in data])
-    return {"review":s1_padded,"label":label}
+    return {"review": s1_padded, "label": label}
